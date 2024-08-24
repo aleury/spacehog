d6ab342cfb87c1dfacb79bc778e6fb3f2d7bd11a
Adam Eury <euryadam@gmail.com>
2024-08-24 08:55:56 -0400

    Add argument parsing to make the path and number of displayed files configurable

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(default_value = ".")]
+    path: String,
+
+    #[arg(short, default_value_t = 5)]
+    number: usize,
+}
+
 fn main() -> std::io::Result<()> {
-    let n = 5;
-    let entries = dstats::list_entries(".")?;
+    let args = Args::parse();
+
+    let n = args.number;
+    let entries = dstats::list_entries(&args.path)?;
 
     println!("*** Top {n} largest files ***");
     for entry in entries.iter().take(n) {
---
+++ tests/cli.rs
@@ -0,0 +1,47 @@
+use assert_cmd::Command;
+
+#[test]
+fn binary_with_help_flag_prints_usage() {
+    Command::cargo_bin("dstats")
+        .unwrap()
+        .arg("--help")
+        .assert()
+        .success()
+        .stdout(predicates::str::contains("Usage: dstats"));
+}
+
+#[test]
+fn binary_with_no_args_prints_top_5_largest_files_under_working_directory() {
+    Command::cargo_bin("dstats")
+        .unwrap()
+        .assert()
+        .success()
+        .stdout(predicates::str::contains("*** Top 5 largest files ***"));
+}
+
+#[test]
+fn binary_with_path_arg_prints_the_top_5_largest_files_under_the_given_path() {
+    let want = vec![
+        "*** Top 5 largest files ***",
+        "   7 B: ./testdata/en/world.txt",
+        "   6 B: ./testdata/es/mundo.txt",
+        "   6 B: ./testdata/en/hello.txt",
+        "   5 B: ./testdata/es/hola.txt",
+    ];
+    Command::cargo_bin("dstats")
+        .unwrap()
+        .arg("./testdata")
+        .assert()
+        .success()
+        .stdout(predicates::str::contains(want.join("\n")));
+}
+
+#[test]
+fn binary_with_the_number_arg_prints_the_top_n_largest_files_under_the_current_working_directory() {
+    Command::cargo_bin("dstats")
+        .unwrap()
+        .arg("-n=10")
+        .assert()
+        .success()
+        .stdout(predicates::str::contains("*** Top 10 largest files ***"));
+}
