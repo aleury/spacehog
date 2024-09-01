8f33539a39437f1f6724695c3d8521265bfc997e
Adam Eury <euryadam@gmail.com>
2024-09-01 12:03:54 -0400

    Simplify find_top_n_largest_files by using a BTreeMap

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod files;
 
-use std::io;
+use std::{collections::BTreeMap, io};
 
 use files::File;
 
@@ -10,21 +10,9 @@ use files::File;
 ///
 /// Returns an I/O error if unable to scan the provided path.
 pub fn find_top_n_largest_files(path: &str, n: usize) -> io::Result<Vec<File>> {
-    let mut results = Vec::with_capacity(n);
-
+    let mut results = BTreeMap::new();
     for file in files::from_path(path)? {
-        if let Some(smallest_file) = results.last() {
-            if results.len() < n || file > *smallest_file {
-                results.push(file);
-                results.sort_by(|a, b| b.cmp(a));
-                if results.len() > n {
-                    results.pop();
-                }
-            }
-        } else {
-            results.push(file);
-        }
+        results.insert(file.clone(), file);
     }
-
-    Ok(results)
+    Ok(results.into_values().rev().take(n).collect())
 }
