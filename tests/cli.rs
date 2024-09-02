6c10085c367d0d56290e5a466188cec65cf03676
Adam Eury <euryadam@gmail.com>
2024-09-02 17:34:09 -0400

    Update

--- tests/cli.rs
+++ tests/cli.rs
@@ -43,10 +43,10 @@ fn binary_with_no_args_prints_top_5_largest_files_under_working_directory() {
 fn binary_with_path_arg_prints_the_top_5_largest_files_under_the_given_path() {
     #[cfg(windows)]
     let want = [
-        "8 B .\\testdata\\en\\world.txt",
-        "7 B .\\testdata\\es\\mundo.txt",
-        "7 B .\\testdata\\en\\hello.txt",
-        "6 B .\\testdata\\es\\hola.txt",
+        "8 B ./testdata\\en\\world.txt",
+        "7 B ./testdata\\es\\mundo.txt",
+        "7 B ./testdata\\en\\hello.txt",
+        "6 B ./testdata\\es\\hola.txt",
     ];
     #[cfg(not(windows))]
     let want = [
