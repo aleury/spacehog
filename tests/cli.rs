04342ff6e06a4ba8ba6e99909cbfabc11ed0b613
Adam Eury <euryadam@gmail.com>
2024-09-03 13:17:25 -0400

    Simplify CLI test (#21)

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,3 +1,5 @@
+use std::io::BufRead;
+
 use assert_cmd::Command;
 
 #[test]
@@ -41,28 +43,19 @@ fn binary_with_no_args_prints_top_5_largest_files_under_working_directory() {
 
 #[test]
 fn binary_with_path_arg_prints_the_top_5_largest_files_under_the_given_path() {
-    #[cfg(windows)]
-    let want = [
-        "*** Top 5 largest files ***",
-        "8 B ./testdata\\en\\world.txt",
-        "7 B ./testdata\\es\\mundo.txt",
-        "7 B ./testdata\\en\\hello.txt",
-        "6 B ./testdata\\es\\hola.txt",
-    ];
-    #[cfg(not(windows))]
-    let want = [
-        "*** Top 5 largest files ***",
-        "7 B ./testdata/en/world.txt",
-        "6 B ./testdata/es/mundo.txt",
-        "6 B ./testdata/en/hello.txt",
-        "5 B ./testdata/es/hola.txt",
-    ];
-    Command::cargo_bin("spacehog")
+    let output = Command::cargo_bin("spacehog")
         .unwrap()
         .arg("./testdata")
         .assert()
         .success()
-        .stdout(predicates::str::contains(want.join("\n")));
+        .get_output()
+        .clone();
+    let lines: Vec<String> = output.stdout.lines().map_while(Result::ok).collect();
+    assert!(lines[0].contains("*** Top 5 largest files ***"));
+    assert!(lines[1].ends_with("world.txt"));
+    assert!(lines[2].ends_with("mundo.txt"));
+    assert!(lines[3].ends_with("hello.txt"));
+    assert!(lines[4].ends_with("hola.txt"));
 }
 
 #[test]
