f68f85edcf472161c130aa8ff2b56a1663df271d
Adam Eury <euryadam@gmail.com>
2024-09-07 16:28:54 -0400

    Use full name for SI units representing bytes (#25)
    * Use full name for SI units representing bytes
    
    * Update humanize test name

--- src/bytes.rs
+++ src/bytes.rs
@@ -8,25 +8,25 @@ pub fn humanize(bytes: u64) -> String {
 const BASE: u64 = 1_000;
 
 enum Unit {
-    B(u64),
-    KB(f64),
-    MB(f64),
-    GB(f64),
-    TB(f64),
-    PB(f64),
-    EB(f64),
+    Bytes(u64),
+    Kilobytes(f64),
+    Megabytes(f64),
+    Gigabytes(f64),
+    Terabytes(f64),
+    Petabytes(f64),
+    Exabytes(f64),
 }
 
 impl Display for Unit {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Unit::B(v) => write!(f, "{v} B"),
-            Unit::KB(v) => write!(f, "{v:.0} KB"),
-            Unit::MB(v) => write!(f, "{v:.1} MB"),
-            Unit::GB(v) => write!(f, "{v:.2} GB"),
-            Unit::TB(v) => write!(f, "{v:.2} TB"),
-            Unit::PB(v) => write!(f, "{v:.2} PB"),
-            Unit::EB(v) => write!(f, "{v:.2} EB"),
+            Unit::Bytes(v) => write!(f, "{v} B"),
+            Unit::Kilobytes(v) => write!(f, "{v:.0} KB"),
+            Unit::Megabytes(v) => write!(f, "{v:.1} MB"),
+            Unit::Gigabytes(v) => write!(f, "{v:.2} GB"),
+            Unit::Terabytes(v) => write!(f, "{v:.2} TB"),
+            Unit::Petabytes(v) => write!(f, "{v:.2} PB"),
+            Unit::Exabytes(v) => write!(f, "{v:.2} EB"),
         }
     }
 }
@@ -34,17 +34,17 @@ impl Display for Unit {
 impl From<u64> for Unit {
     fn from(value: u64) -> Self {
         if value < BASE {
-            return Unit::B(value);
+            return Unit::Bytes(value);
         }
         let exponent = value.ilog10() / BASE.ilog10();
         let value = value as f64 / BASE.pow(exponent) as f64;
         match exponent {
-            1 => Unit::KB(value),
-            2 => Unit::MB(value),
-            3 => Unit::GB(value),
-            4 => Unit::TB(value),
-            5 => Unit::PB(value),
-            _ => Unit::EB(value),
+            1 => Unit::Kilobytes(value),
+            2 => Unit::Megabytes(value),
+            3 => Unit::Gigabytes(value),
+            4 => Unit::Terabytes(value),
+            5 => Unit::Petabytes(value),
+            _ => Unit::Exabytes(value),
         }
     }
 }
@@ -54,7 +54,7 @@ mod test {
     use super::humanize;
 
     #[test]
-    fn humanize_returns_bytes_in_a_human_readable_format() {
+    fn humanize_returns_bytes_in_a_human_readable_format_using_si_units() {
         assert_eq!(humanize(0), "0 B");
         assert_eq!(humanize(256), "256 B");
         assert_eq!(humanize(512), "512 B");
