b34ba9324f35683c77ce7713239956ec32c5401b
Adam Eury <euryadam@gmail.com>
2024-09-08 20:47:42 -0400

    Add a minsize argument to the CLI interface

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,16 @@ struct Args {
 
     #[arg(short, default_value_t = 5)]
     number: usize,
+
+    #[arg(short, default_value_t = 0)]
+    minsize: u64,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let mut count = 0;
     let mut stdout = std::io::stdout();
-    let stream = stream_files_larger_than_min_size(&args.path, args.number, 1_000_000.into())?;
+    let stream = stream_files_larger_than_min_size(&args.path, args.number, args.minsize.into())?;
     while let Ok(results) = stream.recv() {
         count = results.len();
         stdout.queue(cursor::SavePosition)?;
